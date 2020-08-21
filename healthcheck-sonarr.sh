d9bf4f01e55f6c1045470dd31d1e2c6aac5fb953
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2020-08-20 22:23:10 -0600

    Fixed healthcheck.

--- healthcheck-sonarr.sh
+++ healthcheck-sonarr.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
-server_address=$(grep "<BindAddress>.*</BindAddress>" /config/config.xml | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')
+config_path="/config/app/config.xml"
+server_address=$(grep "<BindAddress>.*</BindAddress>" $config_path | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')
 if [[ "$server_address" = "*" ]]
 then
     server_address="localhost"
 fi
 
-server_port=$(grep "<Port>.*</Port>" /config/config.xml | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')
-api_key=$(grep "<ApiKey>.*</ApiKey>" /config/config.xml | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')
-url_base=$(grep "<UrlBase>.*</UrlBase>" /config/config.xml | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')
+server_port=$(grep "<Port>.*</Port>" $config_path | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')
+api_key=$(grep "<ApiKey>.*</ApiKey>" $config_path | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')
+url_base=$(grep "<UrlBase>.*</UrlBase>" $config_path | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')
 if [[ -n $url_base ]]
 then
     url_base="${url_base}/"
@@ -16,10 +17,10 @@ fi
 url="${server_address}:${server_port}/${url_base}api/v3/health?apikey=${api_key}"
 http_url="http://$url"
 
-ssl_enabled=$(grep "<EnableSsl>.*</EnableSsl>" /config/config.xml | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')
+ssl_enabled=$(grep "<EnableSsl>.*</EnableSsl>" $config_path | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')
 if [[ "$ssl_enabled" = "True" ]]
 then
-    ssl_server_port=$(grep "<SslPort>.*</SslPort>" /config/config.xml | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')
+    ssl_server_port=$(grep "<SslPort>.*</SslPort>" $config_path | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')
     ssl_url="https://$url"
 fi
 
